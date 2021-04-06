867d96f12775ad739efe768d7de683f3ff5c2ac4
pranavsai-05 <81681120+pranavsai-05@users.noreply.github.com>
2021-04-06 22:37:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+var player1= Math.floor(Math.random()*6) + 1;
+var player2= Math.floor(Math.random()*6) + 1;
+
+
+
+var randomDiceImg1= "images/dice"+player1+".png";
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImg1);
+
+
+
+var randomDiceImg2= "images/dice"+player2+".png";
+var image2= document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImg2);
+
+if(player1>player2){
+  document.querySelector("h1").innerHTML= "🚩 Player 1 Wins" ;
+}
+else if(player1<player2){
+  document.querySelector("h1").innerHTML= "Player 2 Wins 🚩";
+}
+
+else{
+  document.querySelector("h1").innerHTML= "It's a draw 🙆🏻‍♂️";
+}
